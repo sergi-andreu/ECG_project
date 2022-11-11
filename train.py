0c91c7cd43b18d226299c38d8fe5deac0826061b
Sergi Andreu <sergiandreu97@gmail.com>
2022-11-12 00:09:49 +0100

    added wandb support

--- train.py
+++ train.py
@@ -1,16 +1,24 @@
 import torch
 from .trainutils import *
 
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def train(model, train_dataloader, test_dataloader, lr=5e-4):
+def train(model, train_dataloader, test_dataloader, lr=5e-4, epochs=5, log_every=10, wandb_flag=True):
+    
+    if wandb_flag:
+        import wandb
+        import time
+        run = wandb.init()
+        lr = run.config.lr
+        epochs = run.config.epochs
+        log_every = run.config.log_every
 
     optimizer = torch.optim.Adam(model.parameters(), lr)
+    loss_meter = AverageMeter()
 
     train_ld = {'loss' : []}
     val_ld = {}
-
-    epochs = 5
 
     lossdict = evaluate(test_dataloader, model)
 
@@ -29,6 +37,22 @@ def train(model, train_dataloader, test_dataloader, lr=5e-4):
 
                 loss_meter.update(loss.item())
                 train_ld['loss'].append(loss.item())
+                
+                if wandb_flag:
+                    if i%log_every == 0:
+                        lossdict = evaluate(test_dataloader, model)
+                        wandb.log({"train_loss": loss.item(), 
+                                    "NORM auc": lossdict["auc"][0],
+                                    "MI auc": lossdict["auc"][0],
+                                    "STTC auc": lossdict["auc"][0],
+                                    "CD auc": lossdict["auc"][0],
+                                    "HYP auc": lossdict["auc"][0],
+                                    "NORM auprc": lossdict["auprc"][0],
+                                    "MI auprc": lossdict["auprc"][0],
+                                    "STTC auprc": lossdict["auprc"][0],
+                                    "CD auprc": lossdict["auprc"][0],
+                                    "HYP auprc": lossdict["auprc"][0],
+                                    "val loss": lossdict["epoch_loss"]})
             
             print("Eval at epoch ", epoch)
             lossdict = evaluate(test_dataloader, model)
