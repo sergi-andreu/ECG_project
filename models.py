703b5815c4af151d9aae25afa7daf7de88c506c5
Sergi Andreu <42244939+sergi-andreu@users.noreply.github.com>
2022-11-15 13:09:46 +0100

    Update models.py

--- models.py
+++ models.py
@@ -1,3 +1,29 @@
+"""
+Code for constructing a ResNet model, used for 12-lead ECG data
+author = @Sergi-Andreu
+
+Here only ResNet models are defined, adapted to 1-d signals.
+
+No exhaustive search on the best models for this task has been done.
+
+I have decided to use this sort of models because they seem to be useful for 1d-signal data.
+I possess no domain knowledge on ECG data, nor I have done an exhaustive literature study on the field.
+
+Better models may exist. However, I just wanted to implement ones that "do the job".
+
+This script could be extended to contain more models and do a better hyperparameter / model search.
+
+Some works mentioning ResNet models for this sort of signal data:
+- "ECG Heartbeat Classification Based on ResNet and Bi-LSTM" <https://iopscience.iop.org/article/10.1088/1755-1315/428/1/012014>
+- For ResNet18 and Resnet34 specific models: "Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`
+
+
+This code has been created following the parameters on https://arxiv.org/pdf/1512.03385.pdf , and using multiple repositories
+doing this same task (ResNet18 / ResNet34 for 1d data) for reference. 
+
+"""
+
+# Import required libraries
 import numpy as np
 import os
 import torch
@@ -6,11 +32,16 @@ import torch.utils.data as utils
 import torch.nn.functional as F
 from torch.distributions.normal import Normal
 
+# Define the convolutional layer used (for simplifying the code)
 def conv15x1(in_channels, out_channels, stride=1):
     return nn.Conv1d(in_channels, out_channels, kernel_size=15, stride=stride, padding=7)
 
+# Define the basic block used for the model
 class BasicBlock(nn.Module):
+    
     expansion = 1
+    # The ResNet block has this expansion parameter to act on the number of feature-maps through the conv layer
+    # here set to 1 (as in other works)
 
     def __init__(self, in_channels, out_channels, stride=1, downsample=None):
         super(BasicBlock, self).__init__()
@@ -36,7 +67,6 @@ class BasicBlock(nn.Module):
 
         if self.downsample is not None:
             identity = self.downsample(x)
-        
 
         out += identity
         out = self.relu(out)
@@ -51,8 +81,8 @@ class ResNet(nn.Module):
 
         super(ResNet, self).__init__()
         
-        self.num_outputs = num_outputs
-
+        self.num_outputs = num_outputs # Number of outputs. 
+        # In this specific work it would be the number of labels wanted to be predicted (5)
         self._norm_layer = norm_layer
 
         self.inplanes = 32
@@ -66,6 +96,7 @@ class ResNet(nn.Module):
         self.layer3 = self._make_layer(block, 128, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 256, layers[3], stride=2)
         self.avgpool = nn.AdaptiveAvgPool1d(1)
+        
         self.fc = nn.Sequential(
             nn.Linear(256, num_outputs))
 
@@ -129,21 +160,9 @@ def _resnet(arch, block, layers, pretrained, progress, **kwargs):
 
 
 def resnet18(pretrained=False, progress=True, **kwargs):
-    r"""ResNet-18 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
     return _resnet('resnet18', BasicBlock, [2, 2, 2, 2], pretrained=False, progress=False,
                    **kwargs)
 
 def resnet34(pretrained=False, progress=True, **kwargs):
-    r"""ResNet-34 model from
-    `"Deep Residual Learning for Image Recognition" <https://arxiv.org/pdf/1512.03385.pdf>`_
-    Args:
-        pretrained (bool): If True, returns a model pre-trained on ImageNet
-        progress (bool): If True, displays a progress bar of the download to stderr
-    """
     return _resnet('resnet18', BasicBlock, [3, 4, 6, 3], pretrained=False, progress=False,
                    **kwargs)
